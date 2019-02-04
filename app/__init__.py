ce89c071729acbe56754e69e08eeed29d2b88bce
emma256 <atulindeemmanuel@gmail.com>
2019-02-04 15:02:54 +0300

    Database setup in python

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+app.config['SECRET KEY'] = 'Secret*23@'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://saccoplus:Today*123@127.0.0.1/saccoplus'
 
 
 from app import routes
