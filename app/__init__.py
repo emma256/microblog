c1b31f19a08c4d1e9a98ab1f6db5025f3ec6c95e
emma256 <atulindeemmanuel@gmail.com>
2019-02-07 11:09:27 +0300

    developing a web app with python flask

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,37 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 
 app.config['SECRET KEY'] = 'Secret*23@'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://saccoplus:Today*123@127.0.0.1/saccoplus'
 
+Bootstrap(app)
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login_manager= LoginManager(app)
+
+login_manager.login_message = "You must be logged in to access this page."
+login_manager.login_view = "auth.login"
+    
 
 
-from app import routes,models
+from app import models
+
+
+from app.home import home
+from app.auth import auth
+from app.admin import admin
+
+
+app.register_blueprint(home)
+app.register_blueprint(auth)
+app.register_blueprint(admin)
+
+
+
--- app/models.py
+++ app/models.py
@@ -1,30 +1,85 @@
-from datetime import datetime
-from app import db
+from  werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+from app import db, login_manager
+class User(UserMixin, db.Model):
+    """
+    Create an Employee table
+    """
+
+    # Ensures table will be named in plural and not in singular
+    # as is the name of the model
+    __tablename__ = 'user'
+
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password = db.Column(db.String(128))
-    gender = db.column(db.String(120))
+    email = db.Column(db.String(60), index=True, unique=True)
+    username = db.Column(db.String(60), index=True, unique=True)
+    first_name = db.Column(db.String(60), index=True)
+    last_name = db.Column(db.String(60), index=True)
+    password_hash = db.Column(db.String(128))
+    department_id = db.Column(db.Integer, db.ForeignKey('departments.id'))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    is_admin = db.Column(db.Boolean, default=False)
+
+    @property
+    def password(self):
+        """
+        Prevent pasword from being accessed
+        """
+        raise AttributeError('password is not a readable attribute.')
+
+    @password.setter
+    def password(self, password):
+        """
+        Set password to a hashed password
+        """
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        """
+        Check if hashed password matches actual password
+        """
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return '<Employee: {}>'.format(self.username)
 
-class Roles(db.Model):
+
+# Set up user_loader
+@login_manager.user_loader
+def load_user(user_id):
+    return Employee.query.get(int(user_id))
+
+
+class Department(db.Model):
+    """
+    Create a Department table
+    """
+
+    __tablename__ = 'departments'
+
     id = db.Column(db.Integer, primary_key=True)
-    role = db.Column(db.String(64), index=True, unique=True)
-    permissions = db.Column(db.String(120), index=True, unique=True)
-   
+    name = db.Column(db.String(60), unique=True)
+    description = db.Column(db.String(200))
+    employees = db.relationship('Employee', backref='department',
+                                lazy='dynamic')
 
     def __repr__(self):
-        return '<Role {}>'.format(self.role)
+        return '<Department: {}>'.format(self.name)
 
-class Post(db.Model):
+
+class Role(db.Model):
+    """
+    Create a Role table
+    """
+
+    __tablename__ = 'roles'
+
     id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    name = db.Column(db.String(60), unique=True)
+    description = db.Column(db.String(200))
+    employees = db.relationship('Employee', backref='role',
+                                lazy='dynamic')
 
     def __repr__(self):
-        return '<Post {}>'.format(self.body)
+        return '<Role: {}>'.format(self.name)
